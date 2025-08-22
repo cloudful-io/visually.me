4d8f18abb0fa48df8ebd880cb454df7607c93620
Will Yip <will@cloudful.io>
2025-08-22 16:49:01 -0400

    Removed deprecated code

--- hooks/usePersistedForm.ts
+++ hooks/usePersistedForm.ts
@@ -1,31 +1,3 @@
-//import { useState } from 'react';
-/*
-export function usePersistedForm1<T>(key: string, initialState: T) {
-  const [formValues, setFormValues] = useState<T>(() => {
-    if (typeof window !== 'undefined') {
-      const stored = localStorage.getItem(key);
-      return stored
-        ? JSON.parse(stored, (_key, value) => {
-            // Detect ISO date strings
-            if (typeof value === 'string' && /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/.test(value)) {
-              return new Date(value);
-            }
-            return value;
-          })
-        : initialState;
-    }
-    return initialState;
-  });
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem(key, JSON.stringify(formValues));
-    }
-  }, [formValues]);
-
-  return [formValues, setFormValues] as const;
-}*/
-
 import { useEffect, useState } from 'react';
 import { useForm } from './useForm';
 import { FormFieldConfig } from '@/types/forms';
