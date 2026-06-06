6b207a2fa0907ee4a2a1973f21fe0f1ba5354408
Will Yip <will@cloudful.io>
2026-06-05 20:18:39 -0400

    Removed deprecated service and database type

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -1,7 +1,7 @@
 export type Database = {
   public: {
     Tables: {
-      user_attributes: { Row: { id: string; birth_year: number; retirement_age: number; start_year: number } }
+      userattributes: { Row: { id: string; birth_year: number; retirement_age: number; start_year: number } }
       calculator_stats: { Row: { id: number; calc_count: number; updated_at: string } }
     }
   }
